5fa65269935b1524d7719699672521e680081767
Antoine Dricot <antdricot@gmail.com>
2022-10-04 20:42:34 +0200

    draw character + scale + init

--- game.js
+++ game.js
@@ -17,32 +17,59 @@ function drawRect(context, x, y, width, height, fillStyle, strokeStyle, lineWidt
 
 class GameStates {}
 
+// FIXME another file
+class GameObject {
+    constructor(x=0, y=0, width, height) {
+        console.assert(width);
+        console.assert(height);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(context) {
+        // Hit box for debug purpose
+        drawRect(
+            context, this.x, this.y, this.width, this.height,
+            "purple", "purple", 0)
+    }
+}
+
 export class Game {
+    REFERENCE_HEIGHT = 1000;
     constructor() {
         this.canvas = document.createElement("canvas");
         this.canvas.height = window.innerHeight;
-        this.canvas.width = window.innerWidth;
+        this.canvas.width = window.innerWidth; // FIXME 16:8
         document.body.appendChild(this.canvas);
         this.context = this.canvas.getContext('2d');
-        // TODO scale context/canvas
-        // let factor = this.canvas.width / REFERENCE_SIZE;
-        // this.context.scale(factor, factor);
-        this.state = GameStateStarting;
+        this.scaleFactor = this.canvas.height / Game.REFERENCE_HEIGHT;
+        this.context.scale(this.scaleFactor, this.scaleFactor);
+        this.state = null;
+        this.pc = null;
     }
     draw() {
         this.context.clearRect(0, 0, this.context.canvas.clientWidth, this.context.canvas.clientHeight);
+        // Draw a background (debug)
         if (this.state == GameStateStarting) {
-            this.fillBackground("blue"); // FIXME
+            this.fillCanvasBackground("blue"); // FIXME
         } else if (this.state == GameStateStarted) {
         } else if (this.state == GameStateRunning) {
-            this.fillBackground("yellow");
+            this.fillCanvasBackground("yellow");
         }
+        // Draw playable character
+        this.pc.draw(this.context);
     }
-    fillBackground(color) {
+    fillCanvasBackground(color) {
         drawRect(
             this.context,
             0, 0, this.context.canvas.clientWidth, this.context.canvas.clientHeight,
             color, color, 0);
+    }
+    initialize() {
+        // FIXME overload
+        this.state = GameStateStarting;
+        this.pc = new GameObject(0, 0, 10, 10); // FIXME define width/height somewhere else
     }
     run() {
         this.draw();
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import {Game} from './game.js';
 
 function main() {
     let game = new Game();
+    game.initialize();
     game.run();
 }
 
